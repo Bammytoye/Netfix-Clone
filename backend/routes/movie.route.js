03a417a0b42f67ebf88b5595476dd745d4a113c6
bammytoye <bamigbalatoyese@gmail.com>
2025-05-05 05:18:07 +0100

    'backendMoviesTrending'

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getTrendingMovie } from '../controllers/movie.controller.js';
+import { getMoviesDetails, getMovieTrailers, getTrendingMovie } from '../controllers/movie.controller.js';
 
 const router = express.Router()
 
 router.get('/trending', getTrendingMovie)
+router.get('/:id/trailers', getMovieTrailers)
+router.get('/:id/details', getMoviesDetails)
 
 export default router;
