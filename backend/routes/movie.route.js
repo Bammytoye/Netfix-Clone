ddb3885bf587f9e0c5d8e96f7466f1fd5a23cc2f
bammytoye <bamigbalatoyese@gmail.com>
2025-05-08 09:09:52 +0100

    'tvshows'

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { getMoviesDetails, getMovieTrailers, getTrendingMovie } from '../controllers/movie.controller.js';
+import { getMoviesByCategory, getMoviesDetails, getMovieTrailers, getSimilarMovies, getTrendingMovie } from '../controllers/movie.controller.js';
 
 const router = express.Router()
 
 router.get('/trending', getTrendingMovie)
 router.get('/:id/trailers', getMovieTrailers)
 router.get('/:id/details', getMoviesDetails)
+router.get('/:id/similar', getSimilarMovies)
+router.get('/:category', getMoviesByCategory)
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
 import moviesRoutes from './routes/movie.route.js'
+import tvRoutes from './routes/tv.route.js'
 import { ENV_VARS } from './config/envVars.js'
 import { connectDB } from './config/database.js'
 
@@ -11,6 +12,7 @@ app.use(express.json()); //... for req.body
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/movie', moviesRoutes);
+app.use('/api/v1/tv', tvRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
