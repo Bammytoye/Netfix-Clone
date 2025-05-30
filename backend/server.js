2f66540cf25f0d14137eeea997b7a0b5ac201a59
bammytoye <bamigbalatoyese@gmail.com>
2025-05-30 21:21:20 +0100

    'production'

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/v1/search', protectedRoutes, searchRoutes);
 if(ENV_VARS.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-    app.get('*', (res, req) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     });
 }
